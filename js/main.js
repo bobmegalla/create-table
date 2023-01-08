6541889c785c74b4de3b98eb0338b6826c3122d8
bobmegalla <amakram27@gmail.com>
2023-01-08 13:32:20 +0200

    update

--- js/main.js
+++ js/main.js
@@ -1,8 +1,24 @@
-let btn = document.querySelector("#btn");
+let rowInputs = document.getElementById("rows")
+let colsInputs = document.getElementById("cols")
+let btn = document.querySelector("button")
 
-btn.addEventListener('click', randomBg);
+btn.addEventListener('click',createTable )
 
-function randomBg (){
-    console.log('#' + Math.random().toString(16).slice(2,8))
-    document.body.style.backgroundColor = '#' + Math.random().toString(16).slice(2,8)
+function createTable (){
+    let body = document.body
+    let table = document.createElement("table")
+
+    for (var i =0; i < rowInputs.value;i++) {
+        let tr = document.createElement("tr")
+        table.appendChild(tr)
+        for(var j = 0 ; j < +colsInputs.value;j++){
+        let td = document.createElement("td")
+        let txt = document.createTextNode("item")
+        td.appendChild(txt)
+        tr.appendChild(td)
+        }
+    }
+        body.appendChild(table)
+        rowInputs.value =""
+        colsInputs.value=""
 }
